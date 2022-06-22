6c257bef23b9076eb0e1a033a485593b8fc7d1fb
Alex Garcia <alexsebastian.garcia@gmail.com>
2022-06-22 15:20:46 -0700

    new release workflow

---
+++ .github/workflows/release.js
@@ -0,0 +1,58 @@
+const fs = require("fs").promises;
+
+module.exports = async ({ github, context }) => {
+  const {
+    repo: { owner, repo },
+    sha,
+  } = context;
+  console.log(process.env.GITHUB_REF);
+  const release = await github.rest.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag: process.env.GITHUB_REF.replace("refs/tags/", ""),
+  });
+
+  const release_id = release.data.id;
+  async function uploadReleaseAsset(name, path) {
+    console.log("Uploading", name, "at", path);
+
+    return github.rest.repos.uploadReleaseAsset({
+      owner,
+      repo,
+      release_id,
+      name,
+      data: await fs.readFile(path),
+    });
+  }
+  await Promise.all([
+    uploadReleaseAsset("html0.so", "html0-linux-amd64/html0.so"),
+    uploadReleaseAsset(
+      "html0-linux-amd64-sqlite3",
+      "html0-linux-amd64/sqlite3"
+    ),
+    uploadReleaseAsset(
+      "html0-linux-amd64.zip",
+      "html0-linux-amd64/package.zip"
+    ),
+    uploadReleaseAsset("html0.dylib", "html0-darwin-amd64/html0.dylib"),
+    uploadReleaseAsset(
+      "html0-darwin-amd64-sqlite3",
+      "html0-darwin-amd64/sqlite3"
+    ),
+    uploadReleaseAsset(
+      "html0-darwin-amd64.zip",
+      "html0-darwin-amd64/package.zip"
+    ),
+    uploadReleaseAsset("html0.dll", "html0-windows-amd64/html0.dll"),
+    uploadReleaseAsset(
+      "html0-windows-amd64-sqlite3",
+      "html0-windows-amd64/sqlite3"
+    ),
+    uploadReleaseAsset(
+      "html0-windows-amd64.zip",
+      "html0-windows-amd64/package.zip"
+    ),
+  ]);
+
+  return;
+};
